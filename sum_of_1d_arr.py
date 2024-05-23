9c3fbb2c2dbcd63fd069846bf1736ba937ede272
Jay Mathukiya <68166098+jay-021@users.noreply.github.com>
2024-05-23 17:53:32 +0300

    First program complete this program return sum of 1d arr

--- sum_of_1d_arr.py
+++ sum_of_1d_arr.py
@@ -3,9 +3,10 @@ def sum_of_oneD_arr(arr):
     arr_new = []
     for i in range(0,len(arr)):
         sum += arr[i]
+        # arr_new[i] = sum
         arr_new.append(sum)
         i += 1
-        return arr_new
+    return arr_new
 
 if __name__ == "__main__":
     arr = [1, 2, 3, 4, 5]
