4221c38fec38357ebd1bf12712f6d474e02633d9
Jay Mathukiya <68166098+jay-021@users.noreply.github.com>
2024-05-23 17:44:06 +0300

    Int repo and create first file

---
+++ sum_of_1d_arr.py
@@ -0,0 +1,12 @@
+def sum_of_oneD_arr(arr):
+    sum = 0
+    arr_new = []
+    for i in range(0,len(arr)):
+        sum += arr[i]
+        arr_new.append(sum)
+        i += 1
+        return arr_new
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    print(sum_of_oneD_arr(arr))
